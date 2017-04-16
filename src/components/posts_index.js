1409fce06594e53e6375667aa48c17f4e687e802
Steve Butler <stevebutler2210@gmail.com>
2017-04-16 08:58:21 +0100

    Implemented data fetching via lifecycle method on PostsIndex

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return <div>List of blog posts.</div>
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
 }
+
+// passing { fetchPosts: fetchPosts } or { fetchPosts } === mapPropsToDispatch
+// function to map fetchPosts to props
+export default connect(null, { fetchPosts })(PostsIndex);
