3a314b2e21483f7952aab418fbaf700bbd6472dd
Steve Butler <stevebutler2210@gmail.com>
2017-04-17 09:44:55 +0100

    accessed router via context to implement navigation on submit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  onSubmit(props) {
+    this.props.createPost(props)
+      .then(() => {
+        // blog post successful - return to index
+        // navigate by calling this.context.router.push with the new path
+        this.context.router.push('/');
+      });
+  }
+
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
     
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create a New Post</h3>
         
         <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
@@ -46,7 +59,7 @@ function validate(values) {
   const errors = {};
 
   if (!values.title) {
-    errors.title = 'Enter a username';
+    errors.title = 'Enter a post title';
   }
 
   if (!values.categories) {
