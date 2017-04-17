e98ee94219eef1d541249fc315498f5845ad746f
Steve Butler <stevebutler2210@gmail.com>
2017-04-17 10:48:09 +0100

    added back btn to single post view, and fixed bug causing previous post to flicker when viewing a different post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = { all: [], post: null };
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
     case FETCH_POSTS:
-      return { ...state, all: action.payload.data };
+      return { ...state, all: action.payload.data, post: null };
 
     case FETCH_SINGLE:
       return { ...state, post: action.payload.data };
