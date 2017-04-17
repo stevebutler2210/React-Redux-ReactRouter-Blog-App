4174b625f4eebb6a1cb77f10f3257275a4eb1915
Steve Butler <stevebutler2210@gmail.com>
2017-04-17 10:34:43 +0100

    new action creator for fetching single post, and changes to reducer to handle this

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const FETCH_SINGLE = 'FETCH_SINGLE';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=alnd99las67kdnksdfnsdj3456fkn23113';
@@ -11,6 +12,15 @@ export function fetchPosts() {
 
   return {
     type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+export function fetchSingle(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_SINGLE,
     payload: request
   };
 }
--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSingle } from '../actions/index';
 
 class PostsShow extends Component {
+  componentWillMount() {
+    this.props.fetchSingle(this.props.params.id);
+  }
+
   render() {
-    return <div>Show post {this.props.params.id}</div>
+    return <div>Show post {this.props.params.id}</div>;
   }
 }
 
-export default PostsShow;
+function mapStateToProps(state) {
+  return { post: state.posts.post };
+}
+
+export default connect({ post }, { fetchSingle })(PostsShow);
